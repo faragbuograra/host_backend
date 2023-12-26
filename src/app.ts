e7efa3b245bd04b54e7c8050c86be272c2b290fe
faragbuograra <farag_2516@limu.edu.ly>
2023-12-26 10:43:37 +0200

    .

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,13 @@ import helmet                       from 'helmet'
 
 export const app: Application = express()
 
-app.use(cors())
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions)) 
 
 app.use(express.json());
 app.use(express.static(PUBLIC_PATH))
