ce7c8819dc2b1fe07b217e8a875ab95326b5793a
faragbuograra <farag_2516@limu.edu.ly>
2023-12-26 12:01:12 +0200

    .

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ export const app: Application = express()
 app.use(cors({
     origin: '*'
 }));
-app.use(helmet({crossOriginResourcePolicy: {policy: "cross-origin"}}));
+// app.use(helmet({crossOriginResourcePolicy: {policy: "cross-origin"}}));
 app.use(express.json());
 app.use(express.static(PUBLIC_PATH))
 app.use(express.urlencoded({ extended: true }))
