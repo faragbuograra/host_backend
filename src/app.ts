8a3540e315ac1fb09771f70a4ef4070cc1b88cd4
faragbuograra <farag_2516@limu.edu.ly>
2023-12-26 10:44:55 +0200

    /

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,15 @@ import helmet                       from 'helmet'
 
 export const app: Application = express()
 
+
 const corsOptions ={
     origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
  }
  
- app.use(cors(corsOptions)) 
-
+ app.use(cors(corsOptions))
+// app.use(helmet({crossOriginResourcePolicy: {policy: "cross-origin"}}));
 app.use(express.json());
 app.use(express.static(PUBLIC_PATH))
 app.use(express.urlencoded({ extended: true }))
